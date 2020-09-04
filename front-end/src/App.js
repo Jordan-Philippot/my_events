c4b42a01e1d6d28fea2f3a35d729ded996f590e8
Egzona <egzona.isufi@epitech.eu>
2020-09-04 12:34:33 +0200

    Facebook Connect

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import FacebookLogin from 'react-facebook-login';
+import {Card, Image} from 'react-bootstrap';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [login, setLogin] = useState(false);
+    const [data, setData] = useState({});
+    const [picture, setPicture] = useState('');
+
+    const responseFacebook = (response) => {
+
+        // RESPONSE from Facebook
+        // console.log(response);
+
+        setData(response);
+        setPicture(response.picture.data.url);
+        if (response.accessToken) {
+            setLogin(true);
+        } else {
+            setLogin(false);
+        }
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(response)
+        };
+        fetch('/post', requestOptions);
+    };
+    return (
+        <div className="App">
+            <div className="container">
+                <Card style={{width: '600px'}}>
+                    <Card.Header>
+                        {!login &&
+                        <FacebookLogin
+                            appId="1713722828786731"
+                            autoLoad={true}
+                            fields="name,email,picture"
+                            scope="public_profile,user_friends,email"
+                            callback={responseFacebook}
+                            icon="fa-facebook"/>
+                        }
+                        {login &&
+                        <Image src={picture} roundedCircle/>
+                        }
+                    </Card.Header>
+
+
+                    {login &&
+                    <Card.Body>
+                        <Card.Title>{data.name}</Card.Title>
+                        <Card.Text>
+                            {data.email}
+                        </Card.Text>
+                    </Card.Body>
+                    }
+                </Card>
+            </div>
+        </div>
+    );
 }
 
 export default App;
